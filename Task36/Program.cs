36b0a9ed0156860e3148a203d240bc2aceee6bb8
Alena02cactus <129507128+Alena02cactus@users.noreply.github.com>
2023-05-12 01:03:03 +0300

    решение Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -3,3 +3,43 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int CountElements(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+            counter += arr[i];
+    }
+    return counter;
+}
+
+int[] array = CreateArrayRndInt(4, 1, 100);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] -> ");
+int countElements = CountElements(array);
+Console.WriteLine($"Сумма чисел на нечетных позициях в массиве равно {countElements}");
