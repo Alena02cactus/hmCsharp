aa541940935bfe527a05f55583ba89b14ac04733
Alena02cactus <129507128+Alena02cactus@users.noreply.github.com>
2023-05-12 00:52:17 +0300

    решение Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,3 +2,42 @@
 //  Напишите программу, которая покажет количество чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int CountNumber(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            counter += 1;
+    }
+    return counter;
+}
+
+int[] array = CreateArrayRndInt(5, 100, 1000);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] -> ");
+int countNumber = CountNumber(array);
+Console.WriteLine($"Число четных чисел в массиве равно {countNumber}");
