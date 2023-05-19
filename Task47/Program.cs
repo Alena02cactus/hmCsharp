d9923dc7d73ff0a3e0aee1da86bdf89f53a1fd04
Alena02cactus <129507128+Alena02cactus@users.noreply.github.com>
2023-05-19 13:54:08 +0300

    решение Task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -4,17 +4,16 @@
 // 1   -3,3   8   -9,9
 // 8   7,8   -7,1  9
 
-double[,] CreateMatrixRndInt(double rows, double columns, double min, double max)
+double[,] CreateRndMatrixDouble (int rows, int columns, int min, int max)
 {
-    //                        0       1
-    double[,] matrix = new double[rows, columns]; // 3, 4
     Random rnd = new Random();
-
-    for (double i = 0; i < matrix.GetLength(0); i++) //matrix.GetLength(0) = 3
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (double j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) = 4
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            matrix[i,j] = rnd.NextDouble()*(max-min)+min;
+            matrix[i,j] = Math.Round(matrix[i,j], 2);
         }
     }
     return matrix;
@@ -22,16 +21,16 @@ double[,] CreateMatrixRndInt(double rows, double columns, double min, double max
 
 void PrintMatrix (double[,] matrix)
 {
-    for (double i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
         Console.Write("|");
-        for (double j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j], 5} ");
+            Console.Write($"{matrix[i, j], 5}  ");
         }
-        Console.WriteLine(" |");
+        Console.WriteLine("|");
     }
 }
 
-double[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
-PrintMatrix(array2d);
+double[,] array2D = CreateRndMatrixDouble(4, 4, -100, 100);
+PrintMatrix(array2D);
