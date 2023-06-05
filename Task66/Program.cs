9bd0901696693d2b9799070c1b5dbfd0f09e50dd
Alena02cactus <129507128+Alena02cactus@users.noreply.github.com>
2023-06-06 01:51:53 +0300

    решение Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -3,3 +3,15 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое целое положительное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое положительное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int m, int n)
+{
+    if (m == n) return n;
+    else return SumNumbers(m + 1, n) + m;
+}
+Console.Write(SumNumbers(numberM, numberN));
